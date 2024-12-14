b994427f9823e7c2974ed24fa5207c85b1f2035b
AlgunosSomosAsi <lopezmiguelangel@live.com>
2024-12-13 21:42:09 -0300

    Descripción de los cambios

--- ESP32_Audio_Recorder/ESP32_Audio_Recorder.ino
+++ ESP32_Audio_Recorder/ESP32_Audio_Recorder.ino
@@ -1,5 +1,5 @@
 #include <driver/i2s.h> // I2S Espressif driver (v5.3.1)
-#include <SPI.h>
+#include <SPI.h>        // SD Card used on SPI mode, Arduino module compatible.
 #include <SD.h>
 
 #define LED_BUILTIN       2 
@@ -9,15 +9,16 @@
 #define PM_SDO           17        // PMOD Serial Data OUT
 #define PM_SDIN           5        // PMOD Serial Data IN
 #define sampleRate    44100        // PMOD I2S2 sample rate.
-#define maxAttempts       2        // Todo por triplicado por favor.
+#define maxAttempts       2        // If peripheral INIT fails, try again maxAttempts times.
 #define T_ON       0.5 * 60 * 1000 // Time [minutes] recording.
 #define T_OFF     1 * 60 * 1000000 // Time [minutes] in sleep mode (not recording).
 #define M1                 1000000 // 1 Million.
+
 esp_err_t err;
 File myFile;
 
 void loop() {
-  int i = 0;
+  int i = 0; //Testing.
   Serial.println("Loop start:");
   Serial.println("FS = " + String(sampleRate) + " Hz.");
   record(1000);  // Dummy file.
--- ESP32_Audio_Recorder/Setup.ino
+++ ESP32_Audio_Recorder/Setup.ino
@@ -1,26 +1,9 @@
-/*Micro SD Card connection  VCC 5V    
-  SD_CMD  SS      GPIO  5 (CHIP SELECT)
-  SD_CLK  SCK     GPIO 18 (Default SPI)
-  MOSI    MOSI    GPIO 23 (Default SPI)
-  MISO    MISO    GPIO 21 (Default SPI)
-
-  PMOD I2S2 connection        |        VCC 3.3V
-  01   D/A MCLK     GPIO  0   |   07   A/D MCLK     GPIO  0
-  02   D/A WS       GPIO 25   |   08   A/D WS       GPIO 25
-  03   D/A SCLK     GPIO 26   |   09   A/D SCLK     GPIO 26
-  04   D/A SDOUT    GPIO 22   |   10   A/D SDIN     GPIO 19
-  */
-
-  /* Pasar SD  a GPIO23, 22, 01 y 03
-     Pasar I2S a GPIO0, 4, 2 y 15.
-  */
-
 void setup() {
-  //setCpuFrequencyMhz(240);
-  setup_GIAS_PROT();
+  //setCpuFrequencyMhz(240); // Check for energy efficiency.
+  customSetup();
 }
 
-void setup_I2S() {
+void setup_I2S() { // https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/peripherals/i2s.html
   i2s_config_t i2s_config = {
     .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_TX),  
                               // i2s_mode_t work mode
@@ -44,11 +27,11 @@ void setup_I2S() {
       ;
 
   i2s_pin_config_t pin_config = {
-    .mck_io_num =    PM_MCK,    // GPIO  0 - Limited by hardware. DO NOT CHANGE!
-    .bck_io_num =    PM_BCK,    // GPIO 16
-    .ws_io_num  =    PM_WS,     // GPIO  4
-    .data_out_num =  PM_SDO,    // GPIO 17
-    .data_in_num =   PM_SDIN    // GPIO  5
+    .mck_io_num =    PM_MCK,    // MASTER CLOCK - GPIO  0 - Limited by hardware. DO NOT CHANGE!
+    .bck_io_num =    PM_BCK,    // PMOD SCLK
+    .ws_io_num  =    PM_WS,     // PMOD LRCK
+    .data_out_num =  PM_SDO,    // PMOD SDOUT
+    .data_in_num =   PM_SDIN    // PMOD SDIN
   };
 
     err = i2s_set_pin(I2S_NUM_0, &pin_config);
@@ -76,14 +59,14 @@ void setup_EXIT() {
   esp_deep_sleep_start();  // Sleep mode defined by T_OFF.
 }
 
-void setup_GIAS_PROT() {
-  Serial.begin(115200);                 // Testing
-  setup_I2S();
+void customSetup() {
+  Serial.begin(115200);                 // Testing purposes.
+  setup_I2S();                          // I2S Initialization.
   pinMode(LED_BUILTIN, OUTPUT);         // Visual feedback.
   pinMode(SD_CS, OUTPUT);               // SD SS.
   esp_sleep_enable_timer_wakeup(T_OFF);
   Serial.println("Setup OK");
-  delay(10000);
+  delay(1000);
 }
 
 bool setup_SPI(int SPI_CLK) {
@@ -105,5 +88,3 @@ bool setup_SPI(int SPI_CLK) {
   if (!success) return false;
   return true;
 }
-
-
